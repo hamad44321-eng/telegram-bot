0e43bc27ffad26401b313097bcb3f55d91195518
Ha <hamad44321@gmail.com>
2025-08-09 15:01:35 +0300

    Update keepalive.py

--- keepalive.py
+++ keepalive.py
@@ -19,4 +19,4 @@ def run_bot():
 
 if __name__ == "__main__":
     threading.Thread(target=run_bot, daemon=True).start()
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
